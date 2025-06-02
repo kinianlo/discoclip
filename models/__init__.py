23e9e7fc89887698367de537c31889fc04ebaf4c
Kin Ian Lo <keonlo123@gmail.com>
2025-06-02 19:14:07 +0100

    add text processors

--- models/__init__.py
+++ models/__init__.py
@@ -2,6 +2,20 @@ from .tn_models import (
     EinsumModel,
 )
 
+from .text_processor import (
+    Tokenizer,
+    TextProcessor,
+    BobcatTextProcessor,
+    CupTextProcessor,
+    VectorTextProcessor,
+)
+    
+
 __all__ = [
     "EinsumModel",
+    "Tokenizer",
+    "TextProcessor",
+    "BobcatTextProcessor",
+    "CupTextProcessor",
+    "VectorTextProcessor",
 ]
