a1ce608316407c266fa0ae253b5084438e508aad
Kin Ian Lo <keonlo123@gmail.com>
2025-07-03 11:32:37 +0100

    update

--- setup.py
+++ setup.py
@@ -1,17 +1,15 @@
 from setuptools import setup, find_packages
 
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
 setup(
     name="discoclip",
     version="0.1.0",
     packages=find_packages(),
     description="DiscoClip package for ARO dataset processing",
-    author="Kinian Lo",
-    author_email="",  # Add your email if desired
-    install_requires=[
-        "torch",
-        "pandas",
-        "lambeq",
-        "scikit-learn",
-    ],
+    author="Kin Ian Lo",
+    author_email="kin.lo.20@ucl.ac.uk",
+    install_requires=requirements,
     python_requires=">=3.9",
 )
